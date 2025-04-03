c299b9e17244a9a91026b5334c4ef4ced683f464
opjmelo <jelalk217@gmail.com>
2025-04-03 10:02:07 -0400

    trying to fix

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 # Application definition
 
 INSTALLED_APPS = [
+    'catalog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
